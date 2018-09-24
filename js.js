a5e074a64d8bfdb533c91254edbe119ff72d23b1
Denis Sprinchanu <denchapaev.ua@gmail.com>
2018-09-24 13:09:26 +0300

    давай так

--- js.js
+++ js.js
@@ -1,11 +1,10 @@
- <script src="lib/jquery/jquery.min.js"></script>
 
 
- <script>
-$(document).ready(function(){
-var height = $(window).height();
-$('.header__arrow').on('click',function(){
+
+$(document).ready(() => {
+const height = $(window).height();
+$('.header__arrow').on('click',() => {
    $(window).scrollTop(height);
 })
 });
- </script>
+ 
